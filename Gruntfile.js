5acc79bc9f36d1f8b0d424ec23c81d55ce6c3228
Matan Fintz <matanfintz@gmail.com>
2015-03-25 12:36:19 +0200

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,24 @@
 module.exports = function (grunt) {
 
-// Do grunt‐related things in here
-
+// Do grunt related things in here
+		grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! I am Ugly! */\n',
+				compress: true,
+				mangle: true,
+				sourceMap: true
+			},
+			target: {
+				src: 'src/js/main.js',
+				dest: 'dist/js/main.min.js'
+			}
+		}
+	});
+	//load grunt-contrib-uglify
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	//make it default
+	grunt.registerTask('default', ['uglify']);
+	
 };
